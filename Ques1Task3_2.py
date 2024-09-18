70c3a6dcf23cdc12f6d3c31d3dce9280204683f4
turnayuvraj <yuvrajsinghturna0007@gmail.com>
2024-09-18 22:12:52 +0930

    Update Ques1Task3_2.py

--- Ques1Task3_2.py
+++ Ques1Task3_2.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from transformers import AutoTokenizer
 from collections import Counter
+import warnings 
+warnings.filterwarnings("ignore" , message=".clean_up_tokenization_spaces.")
 
 with open('output.txt', 'r') as file:
     text = file.read()
